5164c6f53f9ccf36709ac0d93c52fe62b15de0c2
hungpham28 <hungpham28092003@gmail.com>
2023-11-16 14:13:32 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
-const newsRouter = require("./news");
+const newsRouter = require('./news')
 
 function route(app) {
-  app.get("/", (req, res) => {
-    res.render("home");
-  });
-      app.post("/search", (req, res) => {
-    res.render("home");
-  });
-  app.get("/search", (req, res) => {
-    res.render("home");
-  });
-  app.use("/news", newsRouter);
+  app.get('/', (req, res) => {
+    res.render('home')
+  })
+            app.post('/search', (req, res) => {
+    res.render('home')
+  })
+  app.get('/search', (req, res) => {
+    res.render('home')
+  })
+  app.use('/news', newsRouter)
 }
-module.exports = route;
+module.exports = route
